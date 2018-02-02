2bbe4f6da9f4ea2b15d3f56601c63e37e295369e
Jürgen <lesmeister.juergen@gmail.com>
2018-02-02 21:09:25 +0100

    add motor regulation

---
+++ README.md
@@ -0,0 +1,5 @@
+# leJOS example project
+
+API docs at http://www.lejos.org/ev3/docs/
+
+gradlew build   Build project
--- src/main/com/jel/mindstorms/Main.java
+++ src/main/com/jel/mindstorms/Main.java
@@ -1,25 +1,35 @@
 package com.jel.mindstorms;
 
+// lejos ev3 api artifact on mavencentral:
 // https://search.maven.org/#search%7Cga%7C1%7Ca%3A%22lejos-ev3-api%22
 
 import lejos.hardware.BrickFinder;
 import lejos.hardware.BrickInfo;
 import lejos.hardware.ev3.EV3;
+import lejos.hardware.Keys;
 import lejos.hardware.lcd.*;
 import lejos.hardware.motor.Motor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.utility.Delay;
 
 public class Main {
 
     public static void main(String[] args) {
-        GraphicsLCD g = BrickFinder.getDefault().getGraphicsLCD();
+        EV3 brick = (EV3)BrickFinder.getDefault();
+        GraphicsLCD g = brick.getGraphicsLCD();
+        Keys buttons = brick.getKeys();
         final int SW = g.getWidth();
         final int SH = g.getHeight();
 
         g.setFont(Font.getDefaultFont());
         g.drawString("Hallo Finn", SW/2, SH/2, GraphicsLCD.BASELINE|GraphicsLCD.HCENTER);
-        Delay.msDelay(2000);
+        buttons.waitForAnyPress();
         g.clear();
         g.refresh();
+
+        EV3LargeRegulatedMotor motorA =
+                new EV3LargeRegulatedMotor(MotorPort.A);
+        motorA.setSpeed(720);
     }
 }
